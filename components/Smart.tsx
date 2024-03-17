ef13734b5f0dd35ae4d9e4e41625d73cbe18f522
Shreyash Kumar Singh <141475077+shing1Sks@users.noreply.github.com>
2024-03-17 22:56:53 +0530

    added text

--- components/Smart.tsx
+++ components/Smart.tsx
@@ -1,5 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
+import { div } from 'three/examples/jsm/nodes/Nodes.js';
+import TypewriterEffectSmoothDemo from './Typewriter';
 
 
 function Smart() {
@@ -90,88 +92,90 @@ function Smart() {
 
     }, [check])
     return (
-        <div className='bg-slate-900 flex flex-wrap items-center gap-6 justify-center p-10'>
-            <div className="card w-60 bg-base-100 shadow-xl h-[420px]">
-                <figure><img className='m-4 h-[140px]' src="https://cdn-icons-png.flaticon.com/512/556/556878.png" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Fan</h2>
-                    <p> Control the speed of the fan - (max speed is 5)</p>
-                    <div className="card-actions flex justify-betwen">
-                        <div>
-                            <button className=' bg-[#9333EA] px-[11px] py-[4px]  rounded-3xl mt-2' onClick={() => setFanSpeed(fanSpeed - 1)}>-</button>
+        <div>
+            <TypewriterEffectSmoothDemo />
+            <div className='bg-slate-900 flex flex-wrap items-center gap-6 justify-center p-10'>
+                <div className="card w-60 bg-base-100 shadow-xl h-[420px]">
+                    <figure><img className='m-4 h-[140px]' src="https://cdn-icons-png.flaticon.com/512/556/556878.png" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Fan</h2>
+                        <p> Control the speed of the fan - (max speed is 5)</p>
+                        <div className="card-actions flex justify-betwen">
+                            <div>
+                                <button className=' bg-[#9333EA] px-[11px] py-[4px]  rounded-3xl mt-2' onClick={() => setFanSpeed(fanSpeed - 1)}>-</button>
+                            </div>
+                            <button onClick={() => { setCheck((prev) => prev + 1); return setFanSpeed(fanSpeed + 1) }} className="btn btn-primary">Speed {fanSpeed}</button>
+                            <button className=' bg-[#9333EA] px-[11px] py-[4px]  rounded-3xl mt-2' onClick={() => setFanSpeed(fanSpeed + 1)}>+</button>
                         </div>
-                        <button onClick={() => { setCheck((prev) => prev + 1); return setFanSpeed(fanSpeed + 1) }} className="btn btn-primary">Speed {fanSpeed}</button>
-                        <button className=' bg-[#9333EA] px-[11px] py-[4px]  rounded-3xl mt-2' onClick={() => setFanSpeed(fanSpeed + 1)}>+</button>
                     </div>
                 </div>
-            </div>
-            <div className="card w-60 bg-base-100 shadow-xl h-[420px]">
-                <figure><img className='m-4 h-[140px]' src="https://cdn-icons-png.flaticon.com/512/8534/8534359.png" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Bulb</h2>
-                    <p>Turn on or off the bulb</p>
-                    <div className="card-actions justify-center">
-                        <button onClick={() => {
-                            setCheck((prev) => prev + 1);
-                            if (bulbOn === "Off") {
-                                setBulb("On");
-                            }
-                            else {
-                                setBulb("Off");
-                            }
-                        }} className="btn btn-primary">State {bulbOn}</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-60 bg-base-100 shadow-xl">
-                <figure><img className='m-4 h-[180px]' src="https://cdn-icons-png.flaticon.com/512/427/427735.png" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Led</h2>
-                    <p>Control the color of the led</p>
-                    <div className="card-actions justify-center">
-                        <button onClick={() => {
-                            setCheck((prev) => prev + 1)
-                            if (color === "#000") {
-                                setColor("#111");
-                            } else {
-                                setColor("#000");
-                            }
-                        }} className="btn btn-primary">Color {color}</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-60 bg-base-100 shadow-xl  h-[420px]">
-                <figure><img className='m-4 h-[140px]' src="https://cdn-icons-png.flaticon.com/512/911/911409.png" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Air Connditioner</h2>
-                    <p>Control state & temp of AC</p>
-                    <div className="card-actions flex items-center justify-between">
-                        <button onClick={() => {
-                            setCheck((prev) => prev + 1);
-                            if (acOn === 0) {
-                                setAc(1);
-                            }
-                            else {
-                                setAc(0);
-                            }
-                        }} className="btn btn-primary ml-[24%]">State {acOn == 0 ? "Off" : "On"}</button>
-                        <div className='flex justify-betwen'>
-                            <div>
-                                <button className=' bg-[#9333EA] px-[11px] py-[4px]  rounded-3xl mt-2' onClick={() => setTemp(temp - 1)}>-</button>
-                            </div>
-                            <div>
-                                <button onClick={() => { setCheck((prev) => prev + 1); return setTemp(temp + 1) }} className="btn btn-primary">Temperature {temp}</button>
-                            </div>
-                            <div>
-                                <button className='bg-purple-600 px-[10px] py-[4px]  rounded-3xl mt-2' onClick={() => setTemp(temp + 1)}>+</button>
-                            </div>
+                <div className="card w-60 bg-base-100 shadow-xl h-[420px]">
+                    <figure><img className='m-4 h-[140px]' src="https://cdn-icons-png.flaticon.com/512/8534/8534359.png" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Bulb</h2>
+                        <p>Turn on or off the bulb</p>
+                        <div className="card-actions justify-center">
+                            <button onClick={() => {
+                                setCheck((prev) => prev + 1);
+                                if (bulbOn === "Off") {
+                                    setBulb("On");
+                                }
+                                else {
+                                    setBulb("Off");
+                                }
+                            }} className="btn btn-primary">State {bulbOn}</button>
                         </div>
+                    </div>
+                </div>
+                <div className="card w-60 bg-base-100 shadow-xl">
+                    <figure><img className='m-4 h-[180px]' src="https://cdn-icons-png.flaticon.com/512/427/427735.png" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Led</h2>
+                        <p>Control the color of the led</p>
+                        <div className="card-actions justify-center">
+                            <button onClick={() => {
+                                setCheck((prev) => prev + 1)
+                                if (color === "#000") {
+                                    setColor("#111");
+                                } else {
+                                    setColor("#000");
+                                }
+                            }} className="btn btn-primary">Color {color}</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-60 bg-base-100 shadow-xl  h-[420px]">
+                    <figure><img className='m-4 h-[140px]' src="https://cdn-icons-png.flaticon.com/512/911/911409.png" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Air Connditioner</h2>
+                        <p>Control state & temp of AC</p>
+                        <div className="card-actions flex items-center justify-between">
+                            <button onClick={() => {
+                                setCheck((prev) => prev + 1);
+                                if (acOn === 0) {
+                                    setAc(1);
+                                }
+                                else {
+                                    setAc(0);
+                                }
+                            }} className="btn btn-primary ml-[24%]">State {acOn == 0 ? "Off" : "On"}</button>
+                            <div className='flex justify-betwen'>
+                                <div>
+                                    <button className=' bg-[#9333EA] px-[11px] py-[4px]  rounded-3xl mt-2' onClick={() => setTemp(temp - 1)}>-</button>
+                                </div>
+                                <div>
+                                    <button onClick={() => { setCheck((prev) => prev + 1); return setTemp(temp + 1) }} className="btn btn-primary">Temperature {temp}</button>
+                                </div>
+                                <div>
+                                    <button className='bg-purple-600 px-[10px] py-[4px]  rounded-3xl mt-2' onClick={() => setTemp(temp + 1)}>+</button>
+                                </div>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
     )
 }
 
