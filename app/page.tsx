be136cfe4bc609fcb74bbb9da80ecca2b052b447
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-03-17 15:00:16 +0530

    updated the footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import  Navbar  from "@/components/Navbar";
 import Gemini from "@/components/Gemini"
 import Smart from "@/components/Smart";
+import Globe from "@/components/Globe";
+import Footer from "@/components/Footer"
 export default function Home() {
   return (
     <main className="bg-black">
       <Navbar />
       <Gemini/>
       <Smart/>
+      <Globe />
+      <Footer />
     </main>
     
   );
